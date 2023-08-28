7ef8d76c0f713546c1766c4c93d628624ecb39de
kirici <74684021+kirici@users.noreply.github.com>
2023-08-28 19:03:42 +0200

    build: multiple templated configs
    currently relying on index, to be moved to a tag or key

--- generate.sh
+++ generate.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-yq 'explode(.) | select(di == 1)' template.pre-commit-config.yaml | tee .pre-commit-config.yaml >/dev/null
+yq 'explode(.) | select(di == 1)' template.pre-commit-config.yaml | tee java-maven/.pre-commit-config.yaml >/dev/null
+yq 'explode(.) | select(di == 2)' template.pre-commit-config.yaml | tee kotlin-maven/.pre-commit-config.yaml >/dev/null
+# yq 'explode(.) | select(di == 3)' template.pre-commit-config.yaml | tee java-gradle/.pre-commit-config.yaml >/dev/null
+# yq 'explode(.) | select(di == 4)' template.pre-commit-config.yaml | tee kotlin-gradle/.pre-commit-config.yaml >/dev/null
