319ec17430ebcebf2d9137ac60b52f43fee1fd73
kirici <74684021+kirici@users.noreply.github.com>
2023-08-28 19:59:17 +0200

    feat: add {java,kotlin}-gradle

--- generate.sh
+++ generate.sh
@@ -2,5 +2,5 @@
 
 yq 'explode(.) | select(di == 1)' template.pre-commit-config.yaml | tee java-maven/.pre-commit-config.yaml >/dev/null
 yq 'explode(.) | select(di == 2)' template.pre-commit-config.yaml | tee kotlin-maven/.pre-commit-config.yaml >/dev/null
-# yq 'explode(.) | select(di == 3)' template.pre-commit-config.yaml | tee java-gradle/.pre-commit-config.yaml >/dev/null
-# yq 'explode(.) | select(di == 4)' template.pre-commit-config.yaml | tee kotlin-gradle/.pre-commit-config.yaml >/dev/null
+yq 'explode(.) | select(di == 3)' template.pre-commit-config.yaml | tee java-gradle/.pre-commit-config.yaml >/dev/null
+yq 'explode(.) | select(di == 4)' template.pre-commit-config.yaml | tee kotlin-gradle/.pre-commit-config.yaml >/dev/null
