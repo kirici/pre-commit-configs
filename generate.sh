3102e02f1670b6c95760e03dac585579486ffb45
kirici <74684021+kirici@users.noreply.github.com>
2023-08-29 19:42:24 +0200

    build: create output dirs as needed

--- generate.sh
+++ generate.sh
@@ -3,5 +3,6 @@ set -euxo pipefail
 declare -a PROJECTS=("merged" "java-maven" "kotlin-maven" "java-gradle" "kotlin-gradle")
 
 for index in "${!PROJECTS[@]}" ; do
+  mkdir -p "${PROJECTS[${index}]}"
   yq 'explode(.) | select(di == '"${index}"')' template.pre-commit-config.yaml | tee "${PROJECTS[${index}]}"/.pre-commit-config.yaml >/dev/null
 done
